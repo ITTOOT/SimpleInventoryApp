84923a84ae57a2f360b7a49d69fe92a5ccb23831
Hayden Williams <hayden@xortech.co.uk>
2022-08-26 21:43:11 +0100

    Latest

--- Models/Base/Instance.cs
+++ Models/Base/Instance.cs
@@ -7,6 +7,7 @@ using System.Threading.Tasks;
 using Dapper.Contrib.Extensions;
 
 using SimpleInventoryApp.Models.Standard;
+using SimpleInventoryApp.Utilities.Attributes;
 
 namespace SimpleInventoryApp.Models.Base
 {
---
+++ Services/IInterfaces/IInstanceRepository.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimpleInventoryApp.Services.IInterfaces
+{
+    public interface IInstanceRepository<IInstance>
+        where IInstance : Instance
+    {
+        Task<int> AddAsync(Task instance);
+    }
+}
